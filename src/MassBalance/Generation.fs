519903517bff521455fefdcbb86d0f89e3af8579
paucablop <pca@specshell.com>
2021-07-31 10:50:11 +0200

    add benchmark

--- src/MassBalance/Generation.fs
+++ src/MassBalance/Generation.fs
@@ -5,8 +5,8 @@ open MathNet.Numerics.LinearAlgebra
 module Generation =
     let stoichionometricMatrix =
         Matrix.Build.DenseDiagonal(2, 4, -1.0)        
-    stoichionometricMatrix.[0, 2] <- 0.51
-    stoichionometricMatrix.[0, 3] <- 0.49
+    stoichionometricMatrix.[0, 2] <- 0.510
+    stoichionometricMatrix.[0, 3] <- 0.490
 
     
     let Calculate (kineticRates: Vector<float>) =
