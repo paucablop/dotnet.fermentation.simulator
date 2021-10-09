93879a75d0899326d500843e9f86a1c65e542e55
Pau Cabaneros <pau.cabaneros@gmail.com>
2021-10-10 00:27:05 +0200

    update benchmark

--- tests/Benchmarks/Benchmarks.fs
+++ tests/Benchmarks/Benchmarks.fs
@@ -2,31 +2,30 @@
 
 open BenchmarkDotNet.Attributes
 open Fermentation.Simulator.Mass.Balance
+open Fermentation.Simulator.Mass.Balance
+open Fermentation.Simulator.Mass.Balance.Stoichiometry
+open Fermentation.Simulator.Process.Model
+open Fermentation.Simulator.Yeast.Anaerobic.Model.YeastUptakeRates
 open MathNet.Numerics.LinearAlgebra
 
 [<MemoryDiagnoser>]
 type Benchmarks () =
-    let initialConditions = Vector<double>.Build.Dense(6, 1.0)
+    let ProcessConditions = ProcessConditions()
+    let InitialConditions = InitialConditions()
+    let InletConcentrations = InletConcentrations()
+    let StoichiometricMatrix = StoichiometricMatrix().Matrix()
+    let YeastUptakeRates = YeastUptakeRates()
     let startTime = 0.0
     let endTime = 10.0
     let timeSteps = 100
-    let stateVariables = initialConditions |> StateVariables
-    let kineticRates = stateVariables |> UptakeRates.Calculate 
-    [<Benchmark>]
-    member this.StateVariable () =
-        initialConditions |> StateVariables
 
     [<Benchmark>]
     member this.UptakeRatesCalculate() =
-        UptakeRates.Calculate(stateVariables)
-        
-    [<Benchmark>]
-    member this.KineticRatesCalculate() =
-        KineticRates.Calculate(kineticRates)
+        UptakeRates.Calculate(InitialConditions, YeastUptakeRates)
         
     [<Benchmark>]
     member this.ProgramSolve() =
-        Simulator.Run(initialConditions, startTime, endTime, timeSteps)
+        SingleBatch.Run(InitialConditions.StateVariablesVector(), startTime, endTime, timeSteps)
         
 
 
